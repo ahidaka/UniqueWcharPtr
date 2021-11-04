15b230159d73186e56079efeea664db3a77253bf
ahidaka <hidaka@devdrv.co.jp>
2021-11-04 23:50:19 +0900

    initial commit

---
+++ Samples/Samples/Samples.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+#include <memory> // std::unique_ptr
+#include <array>
+#include <iostream>
+
+#define NUM_SUB (50)
+
+int Sub(std::array<std::unique_ptr<WCHAR[]>, NUM_SUB> &Subs)
+{
+    PWSTR pw;
+
+    for (size_t i = 0; i < NUM_SUB; i++) {
+        Subs[i] = std::unique_ptr<WCHAR[]>(new WCHAR[MAX_PATH]);
+        pw = (PWSTR)Subs[i].get();
+        _ultow_s(i, pw, MAX_PATH, 10);
+    }
+
+    return NUM_SUB;
+}
+
+int main()
+{
+    std::cout << "Allocate many WCHAR buffers with unique_ptr!\n";
+    std::array<std::unique_ptr<WCHAR[]>, NUM_SUB> subs{};
+
+    Sub(subs);
+    for (auto& e : subs) {
+        //std::cout << e << std::endl;
+        printf("<%ws>\n", e.get());
+    }
+}
